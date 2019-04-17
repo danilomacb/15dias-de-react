6eb32a6a35045f077d8e44b71896284525015516
Danilo <danilo.bakun@gmail.com>
2019-04-17 16:33:36 -0300

    desafios basico e intermediario dia2

---
+++ src/components/NextPlayer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css'
+
+const NextPlayer = () => (
+    <>
+        <h2>Próximo Jogador: </h2>
+        <h1>X</h1>
+    </>
+);
+
+export default NextPlayer;
--- src/components/SquareCollab/index.js
+++ src/components/SquareCollab/index.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./styles.css";
 
-const handleClick = (texto) => console.log(texto);
+const handleClick = () => (
+    console.log("texto")
+);
 
 const SquareCollab = () => (
-    <button onClick={() => handleClick("teste")} className="square-collab">X</button>
+    <button onClick={handleClick} className="square-collab">X</button>
 );
 
 export default SquareCollab;
